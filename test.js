531bec4d7091d269ea965b058bb147dc0ef0ac7e
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2022-06-29 16:23:15 +0530

    added test file

--- test.js
+++ test.js
@@ -1,17 +1,17 @@
-const linkedIn = require('./index');
+const linkedIn = require("./index");
 
 const queryOptions = {
-    keyword: 'software engineer',
-    location: 'los angeles',
-    dateSincePosted: 'past Week',
-    jobType: 'full time',
-    remoteFilter: 'remote',
-    salary: '100000',
-    experienceLevel: 'entry level',
-    limit: '1',
-    sortBy: 'recent'
-  };
-  
-  linkedIn.query(queryOptions).then(response => {
-      console.log(response); // An array of Job objects
-  });
+  keyword: "HR",
+  location: "India",
+  dateSincePosted: "past Week",
+  jobType: "full time",
+  remoteFilter: "remote",
+  salary: "100000",
+  experienceLevel: "entry level",
+  limit: "1",
+  sortBy: "recent",
+};
+
+linkedIn.query(queryOptions).then((response) => {
+  console.log(response); // An array of Job objects
+});
